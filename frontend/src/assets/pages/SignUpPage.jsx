56f5cf10cc69852afc92469f056d59db1a9c5bb7
xNishalShah <nishalshah03@gmail.com>
2025-04-09 22:35:29 +0530

    input width change in signup page

--- frontend/src/assets/pages/SignUpPage.jsx
+++ frontend/src/assets/pages/SignUpPage.jsx
@@ -5,23 +5,23 @@ import { Navbar } from "../components/Navbar";
 export const SignUpPage = () => {
     const [showPassword, setShowPassword] = useState(false)
     return <>
-        <div className="h-[100vh] w-full flex flex-col">
+        <div className="h-[100vh] w-full flex flex-col bg-[url('/images/page3.avif')] bg-cover">
             <div className="h-[13%] relative w-full flex justify-center align-bottom">
                 <Navbar />
             </div>
-            <div className="h-[90%] w-full flex pt-[20px] justify-center items-center bg-[url('/images/main.png')] bg-cover">
-                <div className="w-[50%] h-[90%] flex flex-col px-[40px] pt-[5%] pb-[3%]  justify-between items-center bg-black/80 backdrop-blur-md  border border-white/20 rounded-2xl p-6 shadow-xl">
-                    <div className="w-full h-auto flex flex-col justify-center items-start">
+            <div className="h-[90%] w-full flex pt-[20px] justify-center items-center">
+                <div className="w-[50%] h-[90%] flex flex-col px-[40px] pt-[5%] pb-[3%] justify-between items-center bg-black/20 backdrop-blur-md  border border-white/20 rounded-2xl p-6 shadow-xl">
+                    <div className="w-[70%] h-auto flex flex-col justify-center items-start">
                         <h3 className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] cursor-default font-sans text-xl font-thin">Username</h3>
-                        <input type="text" name="username" id="username" className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] font-sans text-sm font-thin border-white border-b-1 w-[70%] focus:outline-none h-[30px]" />
+                        <input type="text" name="username" id="username" className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] font-sans text-sm font-thin border-white border-b-1 focus:outline-none w-full h-[30px]" />
                     </div>
-                    <div className="w-full h-auto flex flex-col justify-center items-start">
+                    <div className="w-[70%] h-auto flex flex-col justify-center items-start">
                         <h3 className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] cursor-default font-sans text-xl font-thin">Email</h3>
-                        <input type="email" name="email" id="email" className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] font-sans text-sm font-thin border-white border-b-1 w-[70%] focus:outline-none h-[30px]" />
+                        <input type="email" name="email" id="email" className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] font-sans text-sm font-thin border-white border-b-1 w-full focus:outline-none h-[30px]" />
                     </div>
-                    <div className="w-full h-auto flex flex-col justify-center items-start">
+                    <div className="w-[70%] h-auto flex flex-col justify-center items-start">
                         <h3 className="text-white drop-shadow-[0_0_4px_rgba(255,255,255,0.6)] cursor-default font-sans text-xl font-thin">Password</h3>
-                        <div className="relative w-[70%]">
+                        <div className="relative w-full">
                             <input
                                 type={showPassword ? "text" : "password"}
                                 name="password"
